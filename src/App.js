a05cbc8c2ac9207d738d435c57ec5557d652ccaa
Hailey Kim <hailey@Haileys-Laptop.local>
2024-08-28 17:35:40 +0900

    Drop initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import HandIcon from './HandIcon';
+import Dice from './Dice';
+import Button from './Button'
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
+  const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [records, setRecords] = useState([]);
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+    setSum(sum + nextNum);
+    records.push(nextNum);
+  }
+
+  const handleClearClick = () => {
+    setNum();
+    setSum(0);
+    setRecords([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <div>
+        <h2>나</h2>
+        <Dice num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <h2>기록</h2>
+        <p>{records.join(', ')}</p>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/HandIcon.js
@@ -0,0 +1,7 @@
+import Rock from './assets_RSP/rock.svg'
+
+function HandIcon() {
+  return <img src={Rock} alt='주먹' />;
+}
+
+export default HandIcon;
